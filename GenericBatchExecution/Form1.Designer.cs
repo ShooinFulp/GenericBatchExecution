4a1830d64575d1e8fcd9cca83b950fe53c8fd3c4
1349497941@qq.com <13494947941@qq.com>
2019-12-12 17:48:07 +0800

    init code

---
+++ GenericBatchExecution/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace GenericBatchExecution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtSql = new System.Windows.Forms.TextBox();
+            this.btnExec = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(59, 6);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(100, 21);
+            this.txtIp.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ip:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "user:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(250, 6);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 21);
+            this.txtUser.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "pwd:";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(436, 6);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(100, 21);
+            this.txtPwd.TabIndex = 4;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(598, 6);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 6;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtSql
+            // 
+            this.txtSql.Location = new System.Drawing.Point(14, 56);
+            this.txtSql.Multiline = true;
+            this.txtSql.Name = "txtSql";
+            this.txtSql.Size = new System.Drawing.Size(699, 139);
+            this.txtSql.TabIndex = 7;
+            this.txtSql.Text = "if object_id(N\'表名\',N\'U\') is not null\r\nBEGIN\r\n \r\nEND";
+            // 
+            // btnExec
+            // 
+            this.btnExec.Location = new System.Drawing.Point(311, 455);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(75, 23);
+            this.btnExec.TabIndex = 8;
+            this.btnExec.Text = "exec";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(14, 226);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(699, 223);
+            this.txtLog.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "sql:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "log:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 490);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnExec);
+            this.Controls.Add(this.txtSql);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIp);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox txtSql;
+        private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ GenericBatchExecution/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenericBatchExecution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const string GET_DATABASES_NAME = "SELECT NAME FROM MASTER.DBO.SYSDATABASES ORDER BY NAME";
+
+        //所有数据库名
+        private DataTable dt = null;
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                conn = new SqlConnection();
+                conn.ConnectionString = string.Format("server={0};user={1};pwd={2}", txtIp.Text.Trim(), txtUser.Text.Trim(), txtPwd.Text.Trim());
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "连接失败！");
+                return;
+            }
+            SqlDataAdapter adp = new SqlDataAdapter(GET_DATABASES_NAME, conn);
+            dt = new DataTable();
+            adp.Fill(dt);
+            MessageBox.Show("数据库数量：" + dt.Rows.Count + "其中第一个：" + dt.Rows[0][0].ToString(), "连接成功");
+            conn.Close();
+            conn.Dispose();
+        }
+
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+            //清理日志
+            txtLog.Clear();
+            Thread thread = new Thread(new ThreadStart(
+                () =>
+                {
+                    foreach (DataRow item in dt.Rows)
+                    {
+                        string database = item[0].ToString();
+                        string connStr = string.Format("server={0};user={1};pwd={2};database={3};", txtIp.Text.Trim(), txtUser.Text.Trim(), txtPwd.Text.Trim(), database);
+                        try
+                        {
+                            WriteLog("current：" + connStr);
+                            SqlConnection conn = new SqlConnection(connStr);
+                            conn.Open();
+                            SqlCommand cmd = new SqlCommand(txtSql.Text, conn);
+                            int result = cmd.ExecuteNonQuery();
+                            WriteLog("rows：" + result);
+                        }
+                        catch (Exception ex)
+                        {
+                            WriteLog("error：" + ex.Message);
+                        }
+                    }
+                }
+                ));
+            thread.IsBackground = false;
+            thread.Start();
+        }
+
+        private void WriteLog(string msg)
+        {
+            msg = DateTime.Now.ToString() + "   " + msg + "\r\n";
+            if (txtLog.InvokeRequired)
+            {
+                txtLog.Invoke(new Action(() => { txtLog.AppendText(msg); txtLog.ScrollToCaret(); }));
+            }
+            else
+            {
+                txtLog.AppendText(msg);
+                txtLog.ScrollToCaret();
+            }
+        }
+    }
+}
